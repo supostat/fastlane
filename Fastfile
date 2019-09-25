71c839ff08ee17707e0a6a0aabcd69e0838b17d1
Igor Pugachev <ipugachev84@gmail.com>
2019-09-25 08:34:12 +0100

    Initial commit

---
+++ Fastfile
@@ -0,0 +1,267 @@
+fastlane_version "2.53.1"
+fastlane_require "semantic"
+fastlane_require "semantic/core_ext"
+fastlane_require "aws-sdk"
+fastlane_require "byebug"
+fastlane_require "dotenv"
+require "./fastlane_helpers"
+
+PRODUCTION_ENVIRONMENT = 'production'
+DEVELOPMENT_ENVIRONMENT = 'development'
+
+environment = UI.select("Select your environment: ", [PRODUCTION_ENVIRONMENT, DEVELOPMENT_ENVIRONMENT])
+fastlane_helpers = FastlaneHelpers.new(env: environment)
+master_branch = "master"
+
+before_all do
+  Dotenv.load(".env.#{environment}")
+  Dotenv.require_keys(
+    "IOS_PROJECT_FOLDER",
+    "ANDROID_PROJECT_FOLDER",
+    "IOS_APP_NAME",
+    "IOS_PROJECT_FILE_PATH",
+    "IOS_APP_IDENTIFIER",
+    "IOS_PROJECT_SCHEME",
+    "CODE_PUSH_IOS",
+    "CODE_PUSH_ANDROID",
+    "S3_ACCESS_KEY",
+    "S3_SECRET_ACCESS_KEY",
+    "S3_BUCKET",
+    "S3_REGION",
+    "S3_IMAGE_BUCKET",
+    "S3_IMAGE_FOLDER",
+    "S3_IOS_APP_DIR",
+    "S3_ANDROID_APP_DIR",
+    "IOS_PLIST_PATH",
+    "ANDROID_BUILD_GRADLE_PATH",
+    "APP_NAME",
+    "ANDROID_APP_PATH",
+    "ANDROID_APP_SUFFIX"
+  )
+
+  if environment == PRODUCTION_ENVIRONMENT
+    ensure_git_branch(
+      branch: master_branch
+    )
+  end
+
+  ensure_git_status_clean
+  fastlane_helpers.git_fetch
+  fastlane_helpers.change_android_code_push_deployment_key(key: ENV["CODE_PUSH_ANDROID_DEPLOYMENT_KEY"])
+  fastlane_helpers.generate_frontend_env
+end
+
+lane :full_deploy do
+  last_commit_hash = last_git_commit.fetch(:commit_hash)
+  ios_app_url = nil
+  android_app_url = nil
+  begin
+    new_version, tag_name = fastlane_helpers.increment_major_version!
+
+    if git_tag_exists(tag: tag_name)
+      raise "Tag: #{tag_name} already exist."
+    end
+    update_ios_version!(version: new_version)
+    fastlane_helpers.update_android_version!(version: new_version)
+    update_ios_deployment_data
+    update_android_deployment_data
+    fastlane_helpers.git_add_and_commit_all(message: "Version Bump #{new_version}")
+    begin
+      add_git_tag(
+        tag: tag_name,
+      )
+      push_git_tags(tag: tag_name)
+      push_to_git_remote(
+        force: false,
+        force_with_lease: false,
+        tags: false,
+      )
+      images_deploy
+      ios_build
+      s3_deploy_ios
+      ios_app_url = Actions.lane_context[SharedValues::S3_HTML_OUTPUT_PATH]
+      Actions.lane_context[SharedValues::IPA_OUTPUT_PATH] = nil
+      Actions.lane_context[SharedValues::DSYM_OUTPUT_PATH] = nil
+      android_build
+      s3_deploy_android
+      android_app_url = Actions.lane_context[SharedValues::S3_HTML_OUTPUT_PATH]
+      Actions.lane_context[SharedValues::GRADLE_APK_OUTPUT_PATH] = nil
+      ios_code_push_deploy
+      android_code_push_deploy
+      UI.success("Android app can be downloaded at '#{ios_app_url}'")
+      UI.success("iOS app can be downloaded at '#{android_app_url}'")
+    rescue SystemExit, Interrupt => ex
+      puts "Task was stoped by CTRL+C"
+      fastlane_helpers.git_delete_tag(tag: tag_name)
+      fastlane_helpers.git_delete_tag_remote(tag: tag_name)
+      raise ex
+    rescue => ex
+      fastlane_helpers.git_delete_tag(tag: tag_name)
+      fastlane_helpers.git_delete_tag_remote(tag: tag_name)
+      raise ex
+    end
+  rescue SystemExit, Interrupt => ex
+    puts "Task was stoped by CTRL+C"
+    fastlane_helpers.git_reset_hard(hash: last_commit_hash)
+    push_to_git_remote(
+      force: false,
+      force_with_lease: true,
+      tags: false,
+    )
+    fastlane_helpers.remove_frontend_env
+    UI.error(ex.message)
+  rescue => ex
+    fastlane_helpers.git_reset_hard(hash: last_commit_hash)
+    push_to_git_remote(
+      force: true,
+      force_with_lease: false,
+      tags: false,
+    )
+    fastlane_helpers.remove_frontend_env
+    UI.error(ex.message)
+  end
+end
+
+lane :js_deploy do
+  last_commit_hash = last_git_commit.fetch(:commit_hash)
+  begin
+    new_version, tag_name = fastlane_helpers.increment_minor_version!
+    if git_tag_exists(tag: tag_name)
+      raise "Tag: #{tag_name} already exist."
+    end
+    update_ios_deployment_data
+    update_android_deployment_data
+    fastlane_helpers.check_and_update_version_if_development(version: new_version)
+    begin
+      add_git_tag(
+        tag: tag_name,
+      )
+      push_git_tags(tag: tag_name)
+      push_to_git_remote(
+        force: false,
+        force_with_lease: true,
+        tags: false,
+      )
+      ios_code_push_deploy
+      android_code_push_deploy
+    rescue => ex
+      fastlane_helpers.git_delete_tag(tag: tag_name)
+      fastlane_helpers.git_delete_tag_remote(tag: tag_name)
+      raise ex
+    end
+  rescue => ex
+    fastlane_helpers.git_reset_hard(hash: last_commit_hash)
+    push_to_git_remote(
+      force: false,
+      force_with_lease: true,
+      tags: false,
+    )
+    fastlane_helpers.remove_frontend_env
+    UI.error(ex.message)
+  end
+end
+
+lane :s3_deploy_ios do
+  folder = ENV["S3_IMAGE_FOLDER"].empty? ? "" : ENV["S3_IMAGE_FOLDER"] + "/"
+  aws_s3(
+    access_key: ENV["S3_ACCESS_KEY"],
+    secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
+    bucket: ENV["S3_BUCKET"],
+    region: ENV["S3_REGION"],
+    app_directory: ENV["S3_IOS_APP_DIR"],
+    html_template_path: "fastlane/s3_ios_html_template.erb",
+    html_template_params: {
+      base_img_url: "https://#{ENV["S3_IMAGE_BUCKET"]}.s3.#{ENV["S3_REGION"]}.amazonaws.com/#{folder}",
+    },
+  )
+end
+lane :s3_deploy_android do
+  folder = ENV["S3_IMAGE_FOLDER"].empty? ? "" : ENV["S3_IMAGE_FOLDER"] + "/"
+  aws_s3(
+    access_key: ENV["S3_ACCESS_KEY"],
+    secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
+    bucket: ENV["S3_BUCKET"],
+    region: ENV["S3_REGION"],
+    app_directory: ENV["S3_ANDROID_APP_DIR"],
+    html_template_path: "fastlane/s3_android_html_template.erb",
+    html_template_params: {
+      base_img_url: "https://#{ENV["S3_IMAGE_BUCKET"]}.s3.#{ENV["S3_REGION"]}.amazonaws.com/#{folder}",
+    },
+  )
+end
+
+desc "Deploy images for the app download pages"
+private_lane :images_deploy do
+  fastlane_helpers.upload_files_to_s3
+  UI.success("Files uploaded successful")
+end
+
+desc "Fetch certificates and provisioning profiles"
+lane :certificates do
+  match(app_identifier: ENV["IOS_APP_IDENTIFIER"], type: "enterprise")
+end
+
+private_lane :update_ios_deployment_data do
+  provision_profile = "match InHouse #{ENV["IOS_APP_IDENTIFIER"]}"
+  update_app_identifier(
+    xcodeproj: ENV["IOS_PROJECT_FILE_PATH"],
+    plist_path: ENV["IOS_PLIST_PATH"],
+    app_identifier: ENV["IOS_APP_IDENTIFIER"],
+  )
+  update_info_plist(
+    xcodeproj: ENV["IOS_PROJECT_FILE_PATH"],
+    plist_path: ENV["IOS_PLIST_PATH"],
+    block: proc do |plist|
+      plist["CFBundleDisplayName"] = ENV["APP_NAME"]
+      plist["CodePushDeploymentKey"] = ENV["CODE_PUSH_IOS_DEPLOYMENT_KEY"]
+    end,
+  )
+  automatic_code_signing(
+    path: ENV["IOS_PROJECT_FILE_PATH"],
+    use_automatic_signing: false,
+    profile_name: provision_profile,
+  )
+  fastlane_helpers.update_ios_bundle_identifier(app_id: ENV["IOS_APP_IDENTIFIER"], xcodeproj_path: ENV["IOS_PROJECT_FILE_PATH"])
+end
+
+desc "Build the iOS application."
+lane :ios_build do
+  certificates
+  gym(
+    scheme: ENV["IOS_PROJECT_SCHEME"],
+    project: ENV["IOS_PROJECT_FILE_PATH"],
+    output_directory: "builds/",
+    output_name: "#{ENV["IOS_APP_NAME"]}.ipa",
+    silent: true,
+  )
+end
+
+desc "Deploy to Code Push"
+private_lane :ios_code_push_deploy do
+  code_push_release_react(
+    app_name: ENV["CODE_PUSH_IOS"],
+    platform: "ios",
+  )
+end
+
+desc "Deploy Android to Code Push"
+private_lane :android_code_push_deploy do
+  code_push_release_react(
+    app_name: ENV["CODE_PUSH_ANDROID"],
+    platform: "android",
+  )
+end
+
+private_lane :update_android_deployment_data do
+  set_value_in_build(
+    app_project_dir: ENV["ANDROID_APP_PATH"],
+    key: "applicationIdSuffix",
+    value: ENV["ANDROID_APP_SUFFIX"],
+  )
+end
+
+desc "Build the Android application."
+lane :android_build do
+  gradle(task: "clean", project_dir: ENV["ANDROID_PROJECT_FOLDER"])
+  gradle(task: "assemble", build_type: "Release", project_dir: ENV["ANDROID_PROJECT_FOLDER"])
+end
---
+++ fastlane_helpers.rb
@@ -0,0 +1,290 @@
+require "fastlane/action"
+require "fastlane"
+require "aws-sdk"
+require "byebug"
+require "nokogiri"
+
+GIT_LS_LINE_FORMAT = /^[0-9a-f]{40}\trefs\/tags\/\S+$/
+GIT_LS_TAG_NAME_CAPTURE_REGEX = /^[0-9a-f]{40}\trefs\/tags\/(\S+)$/
+PRODUCTION_VERSION_TAG_NAME = /^\d{0,3}\.\d{0,3}\.\d{0,3}$/
+DEVELOPMENT_VERSION_TAG_NAME_REGEX = /^development-\d{0,3}\.\d{0,3}\.\d{0,3}$/
+
+def update_ios_version!(version:)
+  Fastlane::Actions::IncrementVersionNumberAction.run({
+    version_number: version.to_s,
+    xcodeproj: "../#{ENV["IOS_PROJECT_FILE_PATH"]}",
+  })
+end
+
+def get_ios_version
+  Fastlane::Actions::GetVersionNumberAction.run({xcodeproj: "../#{ENV["IOS_PROJECT_FILE_PATH"]}"}).to_version
+end
+
+class FastlaneHelpers
+  PRODUCTION_ENV = "production"
+  DEVELOPMENT_ENV = "development"
+  FRONTEND_ENV_FILE_PREFIX = ".env.frontend"
+  FRONTEND_ENV_PATH = "../.env"
+
+  def initialize(env:)
+    @env = env
+  end
+
+  def generate_frontend_env
+    # Ensure fresh file
+    remove_frontend_env
+
+    temp_env_file = File.new(FRONTEND_ENV_PATH, "w")
+    #env_file
+    File.open("#{FRONTEND_ENV_FILE_PREFIX}.#{env}", "r") do |f|
+      temp_env_file.puts(f.read)
+    end
+    temp_env_file.close
+  end
+
+  def remove_frontend_env
+    File.delete(FRONTEND_ENV_PATH) if File.exist?(FRONTEND_ENV_PATH)
+  end
+
+  def upload_file_to_s3(file_path:)
+    Aws.config.update({
+      region: ENV["S3_REGION"],
+      credentials: Aws::Credentials.new(ENV["S3_ACCESS_KEY"], ENV["S3_SECRET_ACCESS_KEY"]),
+    })
+
+    file_data = File.open(file_path, "rb")
+    s3_client = Aws::S3::Client.new
+    bucket = Aws::S3::Bucket.new(ENV["S3_IMAGE_BUCKET"], client: s3_client)
+    base_file_name = File.basename(file_path)
+    folder = !ENV["S3_IMAGE_FOLDER"].empty? ? "#{ENV["S3_IMAGE_FOLDER"]}/" : ""
+
+    details = {
+      acl: "public-read",
+      key: folder + base_file_name,
+      body: file_data,
+    }
+    obj = bucket.put_object(details)
+    if obj.kind_of? Aws::S3::ObjectVersion
+      obj = obj.object
+    end
+    pp "Uploaded #{obj.public_url.to_s}"
+  end
+
+  def upload_files_to_s3
+    pp "Uploading images to S3 ..."
+    files = Dir["./images/*"]
+    files.each do |file_path|
+      upload_file_to_s3(file_path: file_path)
+    end
+  end
+
+  def update_android_version!(version:)
+    path = "../android/app/build.gradle"
+    re = /versionName\s+"(.*)"/
+
+    build_file = File.read(path)
+    build_file[re, 1] = version.to_s
+
+    f = File.new(path, "w")
+    f.write(build_file)
+    f.close
+  end
+
+  def update_ios_bundle_identifier(app_id:, xcodeproj_path:)
+    path = "../#{xcodeproj_path}/project.pbxproj"
+    p = File.read(path)
+    p.gsub!(/PRODUCT_BUNDLE_IDENTIFIER = .*;/, "PRODUCT_BUNDLE_IDENTIFIER = #{app_id};")
+    File.write(path, p)
+  end
+
+  def git_reset_hard(hash:)
+    Fastlane::Action.sh "git", "reset", "--hard", hash
+  end
+
+  def remote_version_tags
+    result = Fastlane::Action.sh "git", "ls-remote", "--tags", "--refs", "origin" do |status, result, command|
+      unless status.success?
+        raise "Command #{command} (pid #{status.pid}) failed with status #{status.exitstatus}"
+      end
+      lines = result.split("\n")
+
+      # validate format
+      lines.each_with_index do |line, index|
+        if !(line =~ GIT_LS_LINE_FORMAT)
+          raise "Invalid format of git ls-remote at line #{index}\nline: #{line}"
+        end
+      end
+
+      #extract tag names
+      tag_names = lines.map do |line|
+        line.match(GIT_LS_TAG_NAME_CAPTURE_REGEX)[1]
+      end
+
+      tag_names
+    end
+  end
+
+  def extract_production_version_numbers(tag_names:)
+    tag_names.
+      select{ |tag_name| tag_name =~ PRODUCTION_VERSION_TAG_NAME }.
+      map(&:to_version)
+  end
+
+  def extract_development_version_numbers(tag_names:)
+    tag_names.
+      select{ |tag_name| tag_name =~ DEVELOPMENT_VERSION_TAG_NAME_REGEX }.
+      map{ |tag_name| tag_name.gsub('development-', '').to_version }
+  end
+
+  def git_delete_tag(tag:)
+    Fastlane::Action.sh "git", "tag", "-d", tag
+  end
+
+  def git_delete_tag_remote(tag:)
+    Fastlane::Action.sh "git", "push", "-d", "origin", tag
+  end
+
+  def git_fetch
+    Fastlane::Action.sh "git", "fetch"
+  end
+
+  def get_current_version_from_remote
+    all_tag_names = remote_version_tags
+    version_numbers = nil
+
+    case env
+    when PRODUCTION_ENV
+      version_numbers = extract_production_version_numbers(tag_names: all_tag_names)
+    when DEVELOPMENT_ENV
+      version_numbers = extract_development_version_numbers(tag_names: all_tag_names)
+    else
+      raise "Unknown environment"
+    end
+
+    if version_numbers.length === 0
+      version_numbers = ["0.0.1"].to_version
+    end
+
+    version_numbers.sort { |x, y| x <=> y }.last
+  end
+
+  def increment_major_version!
+    current_version = get_current_version_from_remote
+    new_version = current_version.major!
+    write_new_version!(new_version: new_version)
+  end
+
+  def increment_minor_version!
+    current_version = get_current_version_from_remote
+    new_version = current_version.minor!
+    write_new_version!(new_version: new_version)
+  end
+
+  def write_new_version!(new_version:)
+    case env
+    when PRODUCTION_ENV
+      write_package_json_version!(version: new_version)
+      tag_name = new_version
+      [new_version, tag_name.to_string]
+    when DEVELOPMENT_ENV
+      # do nothing, just return new version and tag name, we are using git tags to store development versions
+      tag_name = "development-#{new_version}"
+      [new_version, tag_name]
+    else
+      raise "Unknown environment"
+    end
+  end
+
+  def change_android_code_push_deployment_key(key:)
+    path = "../#{ENV["ANDROID_APP_PATH"]}/src/main/res/values/strings.xml"
+    doc = File.open(path, "r:UTF-8") do |f|
+      @doc = Nokogiri::XML(f)
+      originalName = nil
+      @doc.css("resources string[@name=reactNativeCodePush_androidDeploymentKey]").each do |response_node|
+        originalName = response_node.content
+        response_node.content = key
+        Fastlane::UI.message("Updating codepush deployment key to: #{key}")
+      end
+
+      File.write(path, @doc.to_xml(encoding: "UTF-8"))
+    end
+  end
+
+  def check_and_update_version_if_development(version:)
+    ios_version = get_ios_version
+    android_version = get_android_version
+    if ios_version != android_version
+      raise "iOS and Android versions should match"
+    end
+    if ios_version.major != version.major
+      binary_version = "#{version.major}.0.0".to_version
+      update_ios_version!(version: binary_version)
+      update_android_version!(version: binary_version)
+    end
+    git_add_and_commit_all(message: "Version Bump (v#{version})")
+  end
+
+  def git_status_clean?
+    repo_status = Fastlane::Actions.sh("git status --porcelain")
+    repo_status.empty?
+  end
+
+  def git_add_and_commit_all(message:)
+    unless git_status_clean?
+      staged_files = Fastlane::Action.sh("git", "add", "-A")
+      Fastlane::Action.sh("git", "commit", "-m", message)
+    end
+  end
+
+  private
+  attr_reader :env
+
+  def get_package_json
+    file = File.read("../package.json")
+    JSON.parse(file)
+  end
+
+  def write_package_json!(json:)
+    File.open("../package.json", "w") { |f| f.write JSON.pretty_generate(json) }
+  end
+
+  def write_package_json_version!(version:)
+    package_json = get_package_json
+    package_json["version"] = version.to_s
+    write_package_json!(json: package_json)
+  end
+
+  def read_package_json_version
+    package_json = get_package_json
+    package_json["version"].to_version
+  end
+
+  def increment_package_json_minor_version!
+    current_version = read_package_json_version
+    new_version = current_version.minor!
+    write_package_json_version!(version: new_version)
+    new_version
+  end
+
+  def increment_package_json_major_version!
+    current_version = read_package_json_version
+    new_version = current_version.major!
+    write_package_json_version!(version: new_version)
+    new_version
+  end
+
+  def increment_package_json_patch_version!
+    current_version = read_package_json_version
+    new_version = current_version.patch!
+    write_package_json_version!(version: new_version)
+    new_version
+  end
+
+  def get_android_version
+    path = "../android/app/build.gradle"
+    re = /versionName\s+"(.*)"/
+
+    build_file = File.read(path)
+    build_file[re, 1].to_version
+  end
+end
